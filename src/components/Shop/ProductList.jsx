f3f19226e291c6711000e78d205f277214452873
julypenguin <kaopenguin@gmail.com>
2021-04-03 08:41:27 +0800

    新增 - 點擊商品分類會跳轉到滾動軸最上方

--- src/components/Shop/ProductList.jsx
+++ src/components/Shop/ProductList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { injectIntl, FormattedMessage } from 'react-intl'
 import catData from './categories.json'
 import productData from './products.json'
@@ -8,6 +8,8 @@ const ProductList = ({
     match,
     push,
 }) => {
+    const UlRef = useRef()
+
     const cats_sid = Number(match.params.cats_sid) || 0
     const [newCatData] = catData.categories.filter(category => category.cats_sid === cats_sid)
 
@@ -16,62 +18,66 @@ const ProductList = ({
 
     }
 
+    useEffect(() => {
+        UlRef.current.scrollTo(0, 0)
+    }, [cats_sid])
+
     return (
         <div className='relative h-full pl-4'>
             <h1 className='text-lg font-bold'>{newCatData.name}</h1>
-            <ul className="pr-12 grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 absolute h-full overflow-auto scrollbar scrollbar-thumb-gray-300 scrollbar-track-gray-100">
+            <ul
+                className="pr-12 grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 absolute h-full overflow-auto scrollbar scrollbar-thumb-gray-300 scrollbar-track-gray-100"
+                ref={UlRef}
+            >
                 {newCatData.product.map((id, i) => (
                     <li
                         key={i}
                         className="col-span-1 bg-white rounded-lg divide-gray-200 cursor-pointer flex flex-col"
                         style={{ maxHeight: '400px' }}
                     >
-                        <div className="w-full flex items-center justify-between space-x-6 flex-1">
-                            <div className="flex-1">
-                                <div className="flex items-center space-x-3">
-                                    <div className="text-gray-900 text-sm font-medium">
-                                        {!productData[id] ? null :
-                                            <>
-                                                <div
-                                                    className='w-full flex justify-center'
-                                                    onClick={() => gotoDetail(id)}
-                                                >
-                                                    <div className='relative w-56 pt-56'>
-                                                        <ProductImg
-                                                            img={productData[id].detail && productData[id].detail.product_img && productData[id].detail.product_img[0]}
-                                                        />
-                                                        <span className="flex-shrink-0 inline-block py-2 px-4 text-white text-sm font-medium bg-black absolute top-0 right-0">
-                                                            <FormattedMessage id='shop.discount' />
-                                                        </span>
-                                                    </div>
-
+                        <div className="w-full flex flex-1">
+                            <div className="text-gray-900 text-sm font-medium flex flex-col justify-between">
+                                {!productData[id] ? null :
+                                    <>
+                                        <div>
+                                            <div
+                                                className='w-full flex justify-center mb-4'
+                                                onClick={() => gotoDetail(id)}
+                                            >
+                                                <div className='relative w-56 pt-56'>
+                                                    <ProductImg
+                                                        img={productData[id].detail && productData[id].detail.product_img && productData[id].detail.product_img[0]}
+                                                    />
+                                                    <span className="flex-shrink-0 inline-block py-2 px-4 text-white text-sm font-medium bg-black absolute top-0 right-0">
+                                                        <FormattedMessage id='shop.discount' />
+                                                    </span>
                                                 </div>
 
-                                                <h3
-                                                    className='text-lg text-center'
-                                                    onClick={() => gotoDetail(id)}
-                                                >
-                                                    <FormattedMessage id={`shop.products.${productData[id].intl_id}`} />
-                                                </h3>
+                                            </div>
 
-                                                <h3
-                                                    className='text-center text-red-600'
-                                                    onClick={() => gotoDetail(id)}
-                                                >
-                                                    <span className='mr-4 text-yellow-600 line-through'>
-                                                        <span className='mr-2 text-xl'>NT$</span>
-                                                        <span className='text-xl'>{productData[id].discount}</span>
-                                                    </span>
-                                                    <span>
-                                                        <span className='mr-2 text-xl'>NT$</span>
-                                                        <span className='text-xl'>{productData[id].price}</span>
-                                                    </span>
-                                                </h3>
-                                            </>
-                                        }
-                                    </div>
-                                </div>
-                                {/* <p className="mt-1 text-gray-500 text-sm truncate">Regional Paradigm Technician</p> */}
+                                            <h3
+                                                className='text-lg text-center'
+                                                onClick={() => gotoDetail(id)}
+                                            >
+                                                <FormattedMessage id={`shop.products.${productData[id].intl_id}`} />
+                                            </h3>
+                                        </div>
+
+                                        <h3
+                                            className='text-center text-red-600'
+                                            onClick={() => gotoDetail(id)}
+                                        >
+                                            <span className='mr-4 text-yellow-600 line-through'>
+                                                <span className='mr-2 text-xl'>NT$</span>
+                                                <span className='text-xl'>{productData[id].discount}</span>
+                                            </span>
+                                            <span>
+                                                <span className='mr-2 text-xl'>NT$</span>
+                                                <span className='text-xl'>{productData[id].price}</span>
+                                            </span>
+                                        </h3>
+                                    </>
+                                }
                             </div>
                         </div>
 
